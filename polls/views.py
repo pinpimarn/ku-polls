63e56972b7e4cd7a0321127b088b3c297be170d0
Pinpimarn Pinpisit <pinpimarn.p@ku.th>
2022-09-19 20:16:30 +0700

    Radio button was modified #29

--- polls/views.py
+++ polls/views.py
@@ -38,14 +38,29 @@ class DetailView(LoginRequiredMixin, generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_context_data(self, **kwargs):
+        question = Question.objects.get(pk=self.question_id)
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_authenticated:
+            try:
+                selected_choice = Vote.objects.get(
+                    user=user, choice__in=question.choice_set.all()).choice.choice_text
+                context["selected_choice"] = selected_choice
+            except Vote.DoesNotExist:
+                pass
+        return context
+
     def get(self, request, *args, **kwargs):
         """Send the error message for poll that is not allow for voting,
         but if the poll is allowed for voting, it will send to vote normally."""
-        self.object = Question.objects.filter(pk=kwargs['pk'])[0]
+        self.question_id = kwargs["pk"]
+        self.object = Question.objects.get(pk=self.question_id)
+        if not self.object.is_published():
+            raise Http404("You are not allow to vote on this question")
         if not self.object.can_vote():
             messages.error(request, f'You are not allow to vote on question "{self.object.question_text}"')
-            return HttpResponseRedirect(reverse('polls:index'))
-
+            return redirect("polls:results", pk=self.question_id)
         context = self.get_context_data(object=self.object)
         return self.render_to_response(context)
 
