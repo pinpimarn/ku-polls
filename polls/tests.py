7a5ea89efd5c1026e43657ecb3e2d4e62ba27905
Pinpimarn Pinpisit <pinpimarn.p@ku.th>
2022-09-20 21:18:03 +0700

    Add new test for vote count and authentication #33

--- polls/tests.py
+++ polls/tests.py
@@ -1,9 +1,9 @@
 import datetime
-
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
-from .models import Question
+from .models import Question, Vote
 
 
 def create_question(question_text, days):
@@ -179,3 +179,37 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+    class VoteTest(TestCase):
+        def test_vote_count(self):
+            """Test that the number of votes are counted correctly."""
+            question1 = create_question(question_text='Past Question.', days=-5)
+            choice1 = question1.choice_set.create(choice_text='Correct')
+            Vote.objects.create(question=question1, choice=choice1, user=User.objects.create_user(username='mymelody'))
+            self.assertEqual(1, choice1.votes)
+
+        def test_user_can_vote_one_choice_each_question(self):
+            """Test for one user one vote."""
+            question1 = create_question(question_text='Past Question.', days=-5)
+            choice1 = question1.choice_set.create(choice_text="A lot")
+            choice2 = question1.choice_set.create(choice_text="Not at all")
+            self.client.post(reverse('polls:vote', args=(question1.id,)),
+                             {'choice': choice1.id})
+            self.assertEqual(question1.vote_set.get(user=User.objects.create_user(username='mymelody')).choice,
+                             choice1)
+            self.assertEqual(Vote.objects.all().count(), 1)
+            self.client.post(reverse('polls:vote', args=(question1.id,)),
+                             {'choice': choice2.id})
+            self.assertEqual(question1.vote_set.get(user=User.objects.create_user(username='mymelody')).choice,
+                             choice2)
+            self.assertEqual(Vote.objects.all().count(), 1)
+
+        def test_voter_is_authenticated(self):
+            """Test that only authenticated user can vote."""
+            question1 = create_question(question_text='Past Question.', days=-5)
+            ans_a = self.client.post(reverse('polls:vote', args=(question1.id,)))
+            self.assertEqual(ans_a.status_code, 200)
+            self.client.logout()
+            ans_b = self.client.post(reverse('polls:vote', args=(question1.id,)))
+            self.assertEqual(ans_b.status_code, 302)
+
